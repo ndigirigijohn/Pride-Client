01ea277c58a58f5340c8ac65ef409035c4832d66
jonniek01 <jontejonhn@gmail.com>
2023-01-04 15:32:00 +0300

    setting up carousel

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Home.scss'
+//import Navigation Bar
+import NavBar from '../NavBar/NavBar';
+//import Carousel
+import CarouselRender from './Carousel/CarouselRender';
+
 
 function Home() {
   return (
     <div>
-      
+      <NavBar/>
+      <CarouselRender/>
+
     </div>
   )
 }
